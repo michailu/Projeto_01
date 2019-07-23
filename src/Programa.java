ac243e9625d2656d5e1ae24220d732775efc9fc1
michailu <michailu@gmail.com>
2019-07-23 12:45:38 -0300

    Alterado o gitignore

--- src/Programa.java
+++ src/Programa.java
@@ -3,7 +3,7 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Teste."); //Alterado novamente pelo eclipse
+		System.out.println("Teste."); //Adicionado mais itens ao gitignore
 
 	}
 
